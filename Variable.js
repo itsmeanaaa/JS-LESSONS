e064e1f7842834116a43a5a2ac997dee5d7d4da4
Ana <74693547+itsmeanaaa@users.noreply.github.com>
2024-06-09 11:35:46 +0800

    Create Variable.js

---
+++ Variable.js
@@ -0,0 +1,20 @@
+
+/*let x;
+x=123;
+
+console.log('Hello');
+
+window.alert('this is alert')
+window.alert('I like pizza!');*/
+
+let age=25;
+let price=10.99;
+let gpa=2.1;
+let firstName="Bro";
+
+console.log(`You are ${age} years old`);
+console.log(`The  price if $${price}`);
+console.log(`Your gpa is: ${gpa}`);
+
+console.log(typeof firstName);
+console.log(`You like ${firstName}`);
