2dc4d67cca2e0a7e3f3c97d984ec4c2fe016e784
Ana <74693547+itsmeanaaa@users.noreply.github.com>
2024-06-09 11:23:02 +0800

    Add files via upload

---
+++ JS-1/index.js
@@ -0,0 +1,11 @@
+
+/*let x;
+x=123;
+
+console.log('Hello');
+
+window.alert('this is alert')
+window.alert('I like pizza!');*/
+
+document.getElementById("myH1").textContent='Hello';
+document.getElementById("myP").textContent = 'I like pizza!';
